85e4bb52df05ee70988ee6d8aa76426eb0c8c2b8
Ionray244 <40690531+Ionray244@users.noreply.github.com>
2019-07-23 19:56:23 +1000

    Add files via upload

--- MinesweeperBot.py
+++ MinesweeperBot.py
@@ -6,53 +6,49 @@ import random
 import pprint
 
 pag.FAILSAFE = True
-pag.PAUSE = 0.5
+pag.PAUSE = 0.025
 
-unclickedpng = r"C:\my_python_scripts\MinesweeperBot\unclicked.png"
-clicked_1 = r"c:\my_python_scripts\MinesweeperBot\clicked_1.png"
-clicked_2 = r"C:\my_python_scripts\MinesweeperBot\clicked_2.png"
-clicked_3 = r"C:\my_python_scripts\MinesweeperBot\clicked_3.png"
-clicked_4 = r"C:\my_python_scripts\MinesweeperBot\clicked_4.png"
-clicked_5 = r"C:\my_python_scripts\MinesweeperBot\clicked_5.png"
-clicked_6 = r"C:\my_python_scripts\MinesweeperBot\clicked_6.png"
-face = r"C:\my_python_scripts\MinesweeperBot\face.png"
-flag = r"C:\my_python_scripts\MinesweeperBot\flag.png"
-dead = r"C:\my_python_scripts\MinesweeperBot\gameover.png"
-empty = r"C:\my_python_scripts\MinesweeperBot\emptyclicked.png"
 
-unclicked = list(pag.locateAllOnScreen(unclickedpng))
-board_left = unclicked[0][0]
-board_top = unclicked[0][1]
-board_right = unclicked[-1][0] + 16
-board_bottom = unclicked[-1][1] + 16
-board_width = board_right - board_left
-board_height = board_bottom - board_top
+top_corner = r"C:\my_python_scripts\MinesweeperBot\top_corner.png"
+bottom_corner = r"C:\my_python_scripts\MinesweeperBot\bottom_corner.png"
+
+board_left = int(pag.locateOnScreen(top_corner)[0]+9)
+board_top = int(pag.locateOnScreen(top_corner)[1]+8)
+board_right = int(pag.locateOnScreen(bottom_corner)[0]+9)
+board_bottom = int(pag.locateOnScreen(bottom_corner)[1]+11)
+board_width = board_right - board_left+1
+board_height = board_bottom - board_top+1
 board_space = (board_left, board_top, board_width, board_height)
 
-def click_random():
-    random_choice = random.choice(unclicked)
-    pag.doubleClick(pag.center(random_choice))
-    unclicked.remove(random_choice)
+#Mouse traces around the board, to demonstrate that it has successfully located
+#the game. Then prints out the coordinates and width, height of the game.
+#These numbers can be checked using the mouse_pos script.
+#pag.moveTo(board_left, board_top)
+#pag.moveTo(board_right, board_top, duration=0.5)
+#pag.moveTo(board_right, board_bottom, duration=0.5)
+#pag.moveTo(board_left, board_bottom, duration=0.5)
+#pag.moveTo(board_left, board_top, duration=0.5)
+print(board_space)
 
-#Click 5 random blocks, hopefully until a decent sized open space is created in the grid.
-for i in range(5):
-    click_random()
+#Create the multi-D array to contain the position and type of each block.
+class Block:
+    Position = ()
+    State = int()
 
-#Scan the board and put all block types in their own lists. MAKE THIS INTO A FUNCTION??
-pag.moveTo(board_left - 100)
-empty_blocks = list(pag.locateAllOnScreen(empty))
-ones = list(pag.locateAllOnScreen(clicked_1, region=(board_space)))
-twos = list(pag.locateAllOnScreen(clicked_2, region=(board_space)))
-threes = list(pag.locateAllOnScreen(clicked_3, region=(board_space)))
-fours = list(pag.locateAllOnScreen(clicked_4, region=(board_space)))
-fives = list(pag.locateAllOnScreen(clicked_5, region=(board_space)))
-sixes = list(pag.locateAllOnScreen(clicked_6, region=(board_space)))
+Rows = int(board_height/16)
+Columns = int(board_width/16)
 
-#Sanity check, print the amount of each type of block that is present.
-pprint.pprint(str(len(empty_blocks)) + ' empty block(s)')
-pprint.pprint(str(len(ones)) + ' ones')
-pprint.pprint(str(len(twos)) + ' twos')
-pprint.pprint(str(len(threes)) + ' threes')
-pprint.pprint(str(len(fours)) + ' fours')
-pprint.pprint(str(len(fives)) + ' fives')
-pprint.pprint(str(len(sixes)) + ' sixes')
+Blocks = []
+for i in range(int(Rows)):
+    Blocks.append([])
+    for j in range(Columns):
+        Blocks[i].append([])
+        Blocks[i][j] = Block()
+
+        x = board_left + (16*j)
+        y = board_top + (16*i)
+        
+        Blocks[i][j].Position = (x, y, 16, 16)
+        Blocks[i][j].State = 0
+        pag.moveTo(Blocks[i][j].Position)
+        print(str(Blocks[i][j].Position) + str(Blocks[i][j].State))
